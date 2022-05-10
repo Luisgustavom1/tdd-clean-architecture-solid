8b6549493b438f47149b41d51010a2dfa7ed122d
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-05-10 06:18:58 -0300

    feat: factory, make sut in the tests

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,12 +1,31 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, RenderResult } from '@testing-library/react'
 import Login from './login'
+
+type SutTypes = {
+    sut: RenderResult
+}
+
+const makeSut = (): SutTypes => {
+    const sut = render(<Login />)
+
+    return { sut }
+}
 
 describe('<Login />', () => {
     it('', () => {
-        const { getByTestId } = render(<Login />)
-        const errorWrap = getByTestId('status-wrap')
+        const { sut } = makeSut()
 
+        const errorWrap = sut.getByTestId('status-wrap')
         expect(errorWrap.childElementCount).toBe(0)
+
+        const emailError = sut.getByTestId('email-error')
+        expect(emailError.textContent).toBe('Email obrigatório')
+
+        const passwordError = sut.getByTestId('password-error')
+        expect(passwordError.textContent).toBe('Password obrigatório')
+
+        const submitForm = sut.getByTestId('submit-form') as HTMLButtonElement
+        expect(submitForm.disabled).toBe(true)
     })
 })
