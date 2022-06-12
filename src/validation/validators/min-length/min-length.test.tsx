ed44c7894e041ad1581492d597edf4893af3f7e4
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-06-12 09:30:03 -0300

    refactor: makeSut recieve minLength as parameter and using faker

--- src/validation/validators/min-length/min-length.test.tsx
+++ src/validation/validators/min-length/min-length.test.tsx
@@ -2,18 +2,18 @@ import { MinLengthError } from "@/validation/errors"
 import faker from "@faker-js/faker"
 import { MinLengthValidation } from "./min-length-validation"
 
-const makeSut = () => new MinLengthValidation(faker.random.word(), 5)
+const makeSut = (minLength: number) => new MinLengthValidation(faker.database.column(), minLength)
 
 describe('MinLengthValidation', () => {
   it('Should return error if value is invalid', () => {
-    const sut = makeSut()
+    const sut = makeSut(5)
     const error = sut.validate('')
     expect(error).toEqual(new MinLengthError(sut.minLenght))
   })
 
   it('Should return falsy if value is valid', () => {
-    const sut = makeSut()
-    const error = sut.validate('aloooooo')
+    const sut = makeSut(5)
+    const error = sut.validate(faker.random.alphaNumeric(5))
     expect(error).toBeFalsy()
   })
 })
