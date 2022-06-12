825d6283d24da4667ed0d68211fd90c31f33167c
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-06-12 08:59:46 -0300

    feat: test EmailValidation returns error if email is invalid

---
+++ src/validation/validators/email/email-validation.test.tsx
@@ -0,0 +1,10 @@
+import { InvalidFieldError } from "@/validation/errors"
+import { EmailValidation } from "./email-validations"
+
+describe('EmailValidation', () => {
+  test('Should return error if email is invalid', () => {
+    const sut = new EmailValidation('email')
+    const error = sut.validate('')
+    expect(error).toEqual(new InvalidFieldError())
+  })
+})
