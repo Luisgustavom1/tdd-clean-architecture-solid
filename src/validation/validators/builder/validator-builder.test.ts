a62de2bd29faf5834876b466ba0a0589f197acc2
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-06-12 16:43:52 -0300

    fix: should return emailValidation and should return list of validations

--- src/validation/validators/builder/validator-builder.test.ts
+++ src/validation/validators/builder/validator-builder.test.ts
@@ -3,7 +3,7 @@ import { ValidationBuilder as sut } from "./validator-builder"
 
 describe('ValidationBuilder', () => {
   it('Should return RequiredFieldValidation', () => {
-    const validations = sut.field('any_field').required().build()    
+    const validations = sut.field('any_field').required().build()
     expect(validations).toEqual([new RequireFieldValidation('any_field')])
   })
 
@@ -13,12 +13,12 @@ describe('ValidationBuilder', () => {
   })
 
   it('Should return EmailValidation', () => {
-    const validations = sut.field('any_field').email().build()
+    const validations = sut.field('any_field').minLength(5).build()
     expect(validations).toEqual([new MinLengthValidation('any_field', 5)])
   })
 
-  it('Should return MinLengthValidation and EmailValidation', () => {
-    const validations = sut.field('any_field').email().minLength(5).build()
-    expect(validations).toEqual([new EmailValidation('any_field'), new MinLengthValidation('any_field', 5)])
+  it('Should return a list of validations', () => {
+    const validations = sut.field('any_field').email().required().minLength(5).build()
+    expect(validations).toEqual([new EmailValidation('any_field'), new RequireFieldValidation('any_field'), new MinLengthValidation('any_field', 5)])
   })
 })
