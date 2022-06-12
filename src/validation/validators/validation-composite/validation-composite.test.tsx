369daab434028d59c70da7f1053a02f1f15fe27e
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-06-12 15:55:32 -0300

    test: ensure ValidationComposite returns falsy if there is no error

--- src/validation/validators/validation-composite/validation-composite.test.tsx
+++ src/validation/validators/validation-composite/validation-composite.test.tsx
@@ -1,3 +1,4 @@
+import faker from "@faker-js/faker";
 import { FieldValidationSpy } from "../test/field-validation";
 import { ValidationComposite } from "./validation-composite";
 
@@ -6,10 +7,10 @@ type SutType = {
   fieldValidationsSpy: FieldValidationSpy[]
 }
 
-const makeSut = (): SutType => {
+const makeSut = (fieldName: string): SutType => {
   const fieldValidationsSpy = [
-    new FieldValidationSpy('any_field'),
-    new FieldValidationSpy('any_field')
+    new FieldValidationSpy(fieldName),
+    new FieldValidationSpy(fieldName)
   ]
   const sut = new ValidationComposite(fieldValidationsSpy)
   return {
@@ -20,10 +21,19 @@ const makeSut = (): SutType => {
 
 describe('ValidationComposite', () => {
   it('Should return error if any validation fails', () => {
-    const { sut, fieldValidationsSpy } = makeSut()
-    fieldValidationsSpy[0].error = new Error('first_error_message')
-    fieldValidationsSpy[1].error = new Error('second_error_message')
-    const error = sut.validate('any_field', 'any_value')
-    expect(error).toBe('first_error_message')
+    const fieldName = faker.database.column()
+    const { sut, fieldValidationsSpy } = makeSut(fieldName)
+    const errorMessage = faker.random.words()
+    fieldValidationsSpy[0].error = new Error(errorMessage)
+    fieldValidationsSpy[1].error = new Error(faker.random.words())
+    const error = sut.validate(fieldName, faker.random.words())
+    expect(error).toBe(errorMessage)
+  })
+
+  it('Should not return error if there is no error', () => {
+    const fieldName = faker.database.column()
+    const { sut } = makeSut(fieldName)
+    const error = sut.validate(fieldName, 'any_value')
+    expect(error).toBeFalsy()
   })
 })
