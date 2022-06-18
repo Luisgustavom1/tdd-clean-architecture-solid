dcae82742855125f1087c7d38cf44f81097904ba
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-06-18 09:22:00 -0300

    test: ensure remove readonly on focus

--- src/presentation/components/input/input.test.tsx
+++ src/presentation/components/input/input.test.tsx
@@ -1,17 +1,31 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, RenderResult, screen } from '@testing-library/react'
 import Input from '.'
 import Context from '@/presentation/contexts/form/form-context'
 import { StateErrorsProps, ValuesProps } from '@/presentation/pages/login'
 
+const makeSut = (): RenderResult => {
+  return render(
+    <Context.Provider value={{ stateErrors: {} as StateErrorsProps, setValues: jest.fn(), values: {} as ValuesProps }}>
+      <Input />
+    </Context.Provider>
+  )
+}
+
 describe('<Input />', () => {
-  it('Should make any action begin with readOnly', () => {
-    render(
-      <Context.Provider value={{ stateErrors: {} as StateErrorsProps, setValues: jest.fn(), values: {} as ValuesProps }}>
-        <Input name='field' />
-      </Context.Provider>
-    )
+  it('Should begin with readOnly', () => {
+    makeSut()
 
     expect((screen.getByRole('textbox') as HTMLInputElement).readOnly).toBeTruthy()
+  })
+
+  it('Should remove readOnly on focus', () => {
+    makeSut()
+    
+    const input = screen.getByRole('textbox') as HTMLInputElement;
+
+    fireEvent.focus(input);
+
+    expect(input.readOnly).toBeFalsy()
   })
 })
