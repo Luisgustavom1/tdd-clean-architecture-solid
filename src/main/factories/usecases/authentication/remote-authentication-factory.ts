e0bb3e8ebda5311544c63bd62ec225927eeb946d
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-06-18 09:27:13 -0300

    refactor: pass path as parameter in make api url

--- src/main/factories/usecases/authentication/remote-authentication-factory.ts
+++ src/main/factories/usecases/authentication/remote-authentication-factory.ts
@@ -3,6 +3,6 @@ import { makeApiUrl } from "../../http/api-url-factory";
 import { makeAxiosHttpClient } from "../../http/axios-http-client-factory";
 
 export const makeRemoteAuthentication = () => {
-  const remoteAuthentication = new RemoteAuthentication(makeApiUrl(), makeAxiosHttpClient());
+  const remoteAuthentication = new RemoteAuthentication(makeApiUrl('/login'), makeAxiosHttpClient());
   return remoteAuthentication
 }
