9c38bae2c03c98b3bd1cc21abbd2f8d89a5903e0
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-07-09 11:07:17 -0300

    feat: ensure signup starts with correct initial state

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -8,22 +8,35 @@ import Input from '@/presentation/components/input'
 import Context from '@/presentation/contexts/form/form-context';
 
 import Styles from './signup-style.scss'
+import Spinner from '@/presentation/components/spinner'
+
+type StateErrorsProps = {
+  email: string,
+  password: string
+}
+
+type ValuesProps = {
+  email: string,
+  password: string
+}
 
 const SignUp = () => {
+  const [stateErrors, setStateErrors] = React.useState<StateErrorsProps>({
+    email: 'Campo obrigatório',
+    password: 'Campo obrigatório'
+  })
+  const [mainError, setMainError] = React.useState('');
+  const [isLoading, setIsLoading] = React.useState(false)
+  const [values, setValues] = React.useState<ValuesProps>({
+    email: '',
+    password: ''
+  })
+
   return (
     <div className={Styles.signup}>
       <Header />
 
-      <Context.Provider value={{
-        setValues: () => ({}), stateErrors: {
-          email: 'string',
-          password: 'string'
-        },
-        values: {
-          email: 'string',
-          password: 'string'
-        }
-      }}>
+      <Context.Provider value={{ setValues, stateErrors, values }}>
         <form data-testid='form' className={Styles.form}>
           <h2 className={Styles.formHeader}>Criar conta</h2>
           <Input type='text' name='name' placeholder='Digite seu nome' />
@@ -31,10 +44,18 @@ const SignUp = () => {
           <Input type='password' name='password' placeholder='Digite sua senha' />
           <Input type='password' name='passwordConfirmation' placeholder='Repita sua senha' />
           <div className={Styles.buttonContainer}>
-            <Button data-testid='submit-form' variant='filled' type='submit'>Entrar</Button>
-            <Link to='/login'>
+            <Button data-testid='submit-form' disabled variant='filled' type='submit'>Entrar</Button>
+            <span data-testid='status-wrap'>
+              {isLoading && <Spinner />}
+              {mainError &&
+                <div data-testid='main-error'>
+                  {mainError}
+                </div>
+              }
+            </span>
+            <a href='/login'>
               <Button variant='outlined'>Criar conta</Button>
-            </Link>
+            </a>
           </div>
         </form>
       </Context.Provider>
