8ba73f4de6b49491e38273a1f6beacae4b40c84e
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-07-16 14:03:40 -0300

    fix: export to mock-validation

--- src/presentation/test/mock-validation.ts
+++ src/presentation/test/mock-validation.ts
@@ -1,11 +1,9 @@
 import { Validation } from "../protocols/validations"
 
-class ValidationStub implements Validation {
+export class ValidationStub implements Validation {
     errorMessage: string
 
     validate(fieldName: string, fieldValue: string): string {
         return this.errorMessage
     }
 }
-
-export default ValidationStub
