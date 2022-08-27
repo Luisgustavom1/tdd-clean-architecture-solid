19a665c7a3851e0900dc64abf2b2f7d0f1ca0cad
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-08-27 20:02:53 -0300

    refactor: mockAddAccount

---
+++ src/domain/test/mock-add-account.ts
@@ -0,0 +1,14 @@
+import { AddAccountParams } from "@/domain/usecases/add-account";
+import { faker } from "@faker-js/faker";
+import { AccountModel } from "../model";
+
+export const mockAddAccount = (): AddAccountParams => ({
+    name: faker.database.column(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    passwordConfirmation: faker.internet.password()
+})
+
+export const mockAccountModel = (): AccountModel => ({
+    accessToken: faker.datatype.uuid()
+})
