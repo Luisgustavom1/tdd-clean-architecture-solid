d087ce9921fac2308b158ac0b380098f776f1f80
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-08-27 18:43:10 -0300

    test: add signUpValidationFactory

---
+++ src/main/factories/pages/signup/signup-validation-factory.test.ts
@@ -0,0 +1,15 @@
+import { ValidationComposite } from "@/validation/validators"
+import { ValidationBuilder as Builder } from "@/validation/validators/builder/validator-builder"
+import { makeSignupValidation } from "./signup-validation-factory"
+
+describe('Login', () => {
+  it('Should make ValidationCompose with correct validations', () => {
+    const composite = makeSignupValidation()
+    expect(composite).toEqual(ValidationComposite.build([
+      ...Builder.field('name').required().minLength(5).build(),
+      ...Builder.field('email').required().email().build(),
+      ...Builder.field('password').required().minLength(5).build(),
+      ...Builder.field('passwordConfirmation').required().minLength(5).sameAs('password').build()
+    ]))
+  })
+})
