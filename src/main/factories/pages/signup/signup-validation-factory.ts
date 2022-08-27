44d2f4e6a0e649d180074e7b5371c0026414e898
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-08-27 18:43:00 -0300

    feat: add signUpValidationFactory

---
+++ src/main/factories/pages/signup/signup-validation-factory.ts
@@ -0,0 +1,11 @@
+import { ValidationComposite } from "@/validation/validators";
+import { ValidationBuilder as Builder } from "@/validation/validators/builder/validator-builder";
+
+export const makeSignupValidation = () => {
+  return ValidationComposite.build([
+    ...Builder.field('name').required().minLength(5).build(),
+    ...Builder.field('email').required().email().build(),
+    ...Builder.field('password').required().minLength(5).build(),
+    ...Builder.field('passwordConfirmation').required().minLength(5).sameAs('password').build()
+  ])
+}
