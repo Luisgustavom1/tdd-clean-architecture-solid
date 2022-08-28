6ea005a274f8257f98ae08f912b034d229329821
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-08-28 12:36:38 -0300

    feat: ensure LocalSaveAccessToken throws if no accessToken is provided

--- src/data/usecases/save-acess-token/local-save-access-token.ts
+++ src/data/usecases/save-acess-token/local-save-access-token.ts
@@ -1,10 +1,14 @@
 import { SetStorage } from "@/data/protocols/cache/set-storage";
+import { UnexpectedError } from "@/domain/errors";
 import { SaveAccesssToken } from "@/domain/usecases/save-access-token";
 
 export class LocalSaveAccessToken implements SaveAccesssToken {
   constructor (private readonly setStorage: SetStorage) {}
 
   async save (accessToken: string): Promise<void> {
+    if (!accessToken) {
+      throw new UnexpectedError()
+    }
     this.setStorage.set('accessToken', accessToken)
   }
 }
