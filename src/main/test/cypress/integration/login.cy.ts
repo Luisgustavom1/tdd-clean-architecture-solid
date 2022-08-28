34d1a4e0c557c2b7bceeaad79505a4cfed218958
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-08-28 11:18:19 -0300

    feat: ensure test initial state of login

--- src/main/test/cypress/integration/login.cy.ts
+++ src/main/test/cypress/integration/login.cy.ts
@@ -1,5 +1,11 @@
 describe('Login', () => {
-  it('Should load with correct initial state', () => {
+  beforeEach(() => {
     cy.visit('login')
+  })
+  it('Should load with correct initial state', () => {
+    cy.getByTestId('email-error').contains("Campo obrigatório")
+    cy.getByTestId('password-error').contains("Campo obrigatório")
+    cy.getByTestId('submit-form').should('be.disabled')
+    cy.getByTestId('status-wrap').should('not.have.descendants')
   })
 })
