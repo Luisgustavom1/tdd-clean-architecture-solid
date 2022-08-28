fcf71a8b4b38c21bfe67b66749af2985e91aaa70
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-08-28 11:27:55 -0300

    test: ensure login preset error state if form is invalid

--- src/main/test/cypress/integration/login.cy.ts
+++ src/main/test/cypress/integration/login.cy.ts
@@ -1,10 +1,23 @@
+import { faker } from "@faker-js/faker";
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login')
   })
   it('Should load with correct initial state', () => {
+    cy.getByTestId('email').should('have.attr', 'readonly')
     cy.getByTestId('email-error').contains("Campo obrigatório")
+    cy.getByTestId('password').should('have.attr', 'readonly')
     cy.getByTestId('password-error').contains("Campo obrigatório")
+    cy.getByTestId('submit-form').should('be.disabled')
+    cy.getByTestId('status-wrap').should('not.have.descendants')
+  })
+
+  it('Should present error state if form is invalid', () => {
+    cy.getByTestId('email').focus().type(faker.random.word())
+    cy.getByTestId('email-error').contains("Email inválido")
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(4))
+    cy.getByTestId('password-error').contains("O tamanho mínimo é de 5 caracteres")
     cy.getByTestId('submit-form').should('be.disabled')
     cy.getByTestId('status-wrap').should('not.have.descendants')
   })
