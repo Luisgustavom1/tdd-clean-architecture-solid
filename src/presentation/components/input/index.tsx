b36232d9716d83236f1f86e8fb17ba508ea51fce
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-08-28 12:19:21 -0300

    refactor: add input animations

--- src/presentation/components/input/index.tsx
+++ src/presentation/components/input/index.tsx
@@ -1,32 +1,49 @@
-import React from 'react'
+import React from "react";
 
-import Context from '@/presentation/contexts/form/form-context';
+import Context from "@/presentation/contexts/form/form-context";
 
-import Styles from './styles.scss'
+import Styles from "./styles.scss";
 
-type InputProps = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+type InputProps = React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+>;
 
 const Input = ({ ...props }: InputProps) => {
-    const { stateErrors, setValues, values } = React.useContext(Context)
+  const { stateErrors, setValues, values } = React.useContext(Context);
+  const inputRef = React.useRef(null);
 
-    const error = stateErrors[`${props.name}Error`]
+  const error = stateErrors[`${props.name}Error`];
 
-    const enableInput = (e: React.FocusEvent<HTMLInputElement>) => {
-        e.target.readOnly = false
-    }
+  const enableInput = (e: React.FocusEvent<HTMLInputElement>) => {
+    e.target.readOnly = false;
+  };
 
-    const handleChange = (e: React.FocusEvent<HTMLInputElement>) => {
-        setValues({
-            ...values,
-            [e.target.name]: e.target.value
-        })
-    }
-    return (
-        <>
-            <input onChange={handleChange} onFocus={enableInput} data-testid={props.name} readOnly {...props} />
-            <span data-testid={`${props.name}-error`} className={Styles.errorMessage}>{error}</span>
-        </>
-    )
-}
+  const handleChange = (e: React.FocusEvent<HTMLInputElement>) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+  return (
+    <>
+      <div className={Styles["input-wrap"]}>
+        <input
+          {...props}
+          onChange={handleChange}
+          onFocus={enableInput}
+          data-testid={props.name}
+          readOnly
+          placeholder=" "
+          ref={inputRef}
+        />
+        <label onClick={() => inputRef.current.focus()}>{props.placeholder}</label>
+      </div>
+      <span data-testid={`${props.name}-error`} className={Styles.errorMessage}>
+        {error}
+      </span>
+    </>
+  );
+};
 
-export default Input
+export default Input;
