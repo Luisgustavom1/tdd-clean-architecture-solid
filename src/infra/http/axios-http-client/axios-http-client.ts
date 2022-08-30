49682c05daf636a68c6a029e17e3a8e7182d71d2
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-08-30 08:23:42 -0300

    feat: ensure AxiosHttpClient returns correct value on success

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,7 +1,7 @@
-import { HttpGetParams, HttpPostClient, HttpPostParams, HttpResponse } from "@/data/protocols/http";
+import { HttpGetClient, HttpGetParams, HttpPostClient, HttpPostParams, HttpResponse } from "@/data/protocols/http";
 import axios, { AxiosResponse } from "axios";
 
-export class AxiosHttpClient implements HttpPostClient<any, any> {
+export class AxiosHttpClient implements HttpPostClient<any, any>, HttpGetClient {
     async post(params: HttpPostParams<any>): Promise<HttpResponse<any>> {
         let axiosResponse: AxiosResponse<any>
 
@@ -18,7 +18,11 @@ export class AxiosHttpClient implements HttpPostClient<any, any> {
         }
     }
 
-    async get (params: HttpGetParams): Promise<void> {
-        await axios.get(params.url)
+    async get (params: HttpGetParams): Promise<HttpResponse> {
+        const axiosResponse = await axios.get(params.url)
+        return {
+            statusCode: axiosResponse.status,
+            body: axiosResponse.data
+        };
     }
 }
