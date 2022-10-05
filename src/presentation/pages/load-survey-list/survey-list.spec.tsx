33f29be84ecb7177876178be545d2ed09958a9e6
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-10-04 21:50:37 -0300

    test: ensure SurveyList component calls

--- src/presentation/pages/load-survey-list/survey-list.spec.tsx
+++ src/presentation/pages/load-survey-list/survey-list.spec.tsx
@@ -1,15 +1,40 @@
 import { render, screen } from "@testing-library/react";
 import SurveyList from "./survey-list";
 import React from "react";
+import { LoadSurveyList } from "@/domain/usecases";
+import { SurveyModel } from "@/domain/model";
 
-const makeSut = () => {
-  render(<SurveyList />)
+class LoadSurveyListSpy implements LoadSurveyList {
+  callsCount = 0;
+
+  async loadAll(): Promise<SurveyModel[]> {
+    this.callsCount++;
+    return [];
+  }
 }
+
+type SutTypes = {
+  loadSurveyListSpy: LoadSurveyListSpy;
+};
+
+const makeSut = (): SutTypes => {
+  const loadSurveyListSpy = new LoadSurveyListSpy();
+  render(<SurveyList loadSurveyList={loadSurveyListSpy} />);
+
+  return {
+    loadSurveyListSpy,
+  };
+};
 
 describe("SurveyList Component", () => {
   it("Should present 4 empty items on start", () => {
     makeSut();
     const surveyList = screen.getByTestId("survey-list");
     expect(surveyList.querySelectorAll("li:empty").length).toBe(4);
+  });
+
+  it("Should call LoadSurveyList", () => {
+    const { loadSurveyListSpy } = makeSut();
+    expect(loadSurveyListSpy.callsCount).toBe(1);
   });
 });
