327364fbb28cb2d2ed236ecae2ff9e9ccfb2d206
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-10-04 22:57:03 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/pages/load-survey-list/components/survey-item/survey-item.spec.tsx
+++ src/presentation/pages/load-survey-list/components/survey-item/survey-item.spec.tsx
@@ -4,19 +4,24 @@ import { SurveyItem } from ".";
 import React from "react";
 import { IconName } from "@/presentation/components/icon";
 
+const makeSut = (survey = mockSurveyModel()) => {
+  render(<SurveyItem survey={survey} />);
+};
+
 describe("SurveyItem component", () => {
   it("Should render with correct values", () => {
-    const survey = mockSurveyModel();
-    survey.didAnswer = true;
-    survey.date = new Date("2022-01-10T00:00:00");
-    render(<SurveyItem survey={survey} />);
+    const survey = Object.assign(mockSurveyModel(), {
+      didAnswer: true,
+      date: new Date("2022-01-10T00:00:00"),
+    });
+    makeSut(survey);
     expect(screen.getByTestId("icon")).toHaveProperty(
       "src",
       IconName.thumbDown
     );
     expect(screen.getByTestId("question").textContent).toBe(survey.question);
-    expect(screen.getByTestId("day").textContent).toBe('10');
-    expect(screen.getByTestId("month").textContent).toBe('jan');
-    expect(screen.getByTestId("year").textContent).toBe('2022');
+    expect(screen.getByTestId("day").textContent).toBe("10");
+    expect(screen.getByTestId("month").textContent).toBe("jan");
+    expect(screen.getByTestId("year").textContent).toBe("2022");
   });
 });
