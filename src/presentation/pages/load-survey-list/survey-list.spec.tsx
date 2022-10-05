2d68ab6c99b63b72f31abaf9ea7b01bb38b1e701
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-10-04 21:42:21 -0300

    refactor: move sut to a crate factory method

--- src/presentation/pages/load-survey-list/survey-list.spec.tsx
+++ src/presentation/pages/load-survey-list/survey-list.spec.tsx
@@ -2,9 +2,13 @@ import { render, screen } from "@testing-library/react";
 import SurveyList from "./survey-list";
 import React from "react";
 
+const makeSut = () => {
+  render(<SurveyList />)
+}
+
 describe("SurveyList Component", () => {
   it("Should present 4 empty items on start", () => {
-    render(<SurveyList />);
+    makeSut();
     const surveyList = screen.getByTestId("survey-list");
     expect(surveyList.querySelectorAll("li:empty").length).toBe(4);
   });
