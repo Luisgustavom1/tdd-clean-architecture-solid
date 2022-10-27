ff749eae547f9a95cf0bcba90e4cf734857efec1
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-10-27 08:44:27 -0300

    feat: ensure AuthorizeHttpGetClientDecorator merge token to HttpgetClient

--- src/main/decorators/authorize-http-get-client-deccorator/index.ts
+++ src/main/decorators/authorize-http-get-client-deccorator/index.ts
@@ -10,11 +10,9 @@ export class AuthorizeHttpGetClientDecorator implements HttpGetClient {
   async get(params: HttpGetParams): Promise<HttpResponse<any>> {
     const account = this.getStorage.get('account')
     if (account?.accessToken) {
-      Object.assign(params, {
-        headers: {
-          'x-access-token': account.accessToken
-        }
-      })
+      params.headers = Object.assign({
+        'x-access-token': account.accessToken
+      }, params.headers)
     }
     await this.httpGetClient.get(params)
     return null
