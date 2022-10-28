cd866afd91173b685b61d6194912e47603899820
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-10-28 08:42:59 -0300

    feat: add authorizeHttpGetClientDecoratorFactory

--- src/main/factories/usecases/load-survey-list/remote-load-survey-list-factory.ts
+++ src/main/factories/usecases/load-survey-list/remote-load-survey-list-factory.ts
@@ -1,7 +1,7 @@
 import { RemoteLoadSurveyList } from "@/data/usecases/load-survey-list/remote-load-survey-list"
 import { makeApiUrl } from "@/main/factories/http/api-url-factory"
-import { makeAxiosHttpClient } from "@/main/factories/http/axios-http-client-factory"
+import { makeAuthorizeHttpGetClientDecorator } from "@/main/factories/decorators/authorize-http-get-client-decorator-factory"
 
 export const makeRemoteSurveyList = () => {
-  return new RemoteLoadSurveyList(makeApiUrl('/surveys'), makeAxiosHttpClient())
+  return new RemoteLoadSurveyList(makeApiUrl('/surveys'), makeAuthorizeHttpGetClientDecorator())
 }
