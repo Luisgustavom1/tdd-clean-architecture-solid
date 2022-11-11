348fdc8edf6f76dbb65e5401cd652d4c5d4cfc24
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-11-10 22:06:20 -0300

    fix: file name

--- src/data/usecases/load-survey-list/remote-load-survey-list.ts
+++ src/data/usecases/load-survey-list/remote-load-survey-list.ts
@@ -1,5 +1,5 @@
 import { HttpGetClient, HttpStatusCode } from "@/data/protocols/http";
-import { UnexpectedError } from "@/domain/errors";
+import { AccessDeniedError, UnexpectedError } from "@/domain/errors";
 import { LoadSurveyList } from "@/domain/usecases";
 
 export class RemoteLoadSurveyList implements LoadSurveyList {
@@ -19,6 +19,7 @@ export class RemoteLoadSurveyList implements LoadSurveyList {
         date: new Date(remoteSurvey.date)
       }));
       case HttpStatusCode.noContent: return [];
+      case HttpStatusCode.forbidden: throw new AccessDeniedError()
       default: throw new UnexpectedError()
     }
   }
