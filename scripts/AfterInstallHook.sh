e4460fe1d3c954eafe71a9bcb16a8d4987bce7e8
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-12-04 15:16:56 -0300

    refactor: update AfterInstall script

--- scripts/AfterInstallHook.sh
+++ scripts/AfterInstallHook.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 set -e
+
+# preparing setup
+sudo apt-get install wget
+
+# install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+
+# install node version
+nvm install 16.18.1
+nvm use 16.18.1
+node --version
+
+# run appa
 node index.js
