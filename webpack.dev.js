4b169e23db0947bc03596d45d6598f5584e0d8bb
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-12-04 20:01:49 -0300

    chore: dump cypress version, add start-server-and-test and add new scripts

--- webpack.dev.js
+++ webpack.dev.js
@@ -33,7 +33,8 @@ module.exports = merge(common, {
     historyApiFallback: true,
     devMiddleware: {
       writeToDisk: true
-    }
+    },
+    port: 8080
   },
   plugins: [
     new DefinePlugin({
