4f8713f55af1e5b477f46f5f7597ecb0af6826e5
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-12-04 15:51:19 -0300

    refactor: update AfterInstall script

--- scripts/AfterInstallHook.sh
+++ scripts/AfterInstallHook.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 set -e
 
-# install nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
-. ~/.nvm/nvm.sh
-
 # install node version
-nvm install 16.18.1
 nvm use 16.18.1
 node --version
 
