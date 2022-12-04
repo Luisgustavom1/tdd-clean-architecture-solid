1649d63dccdae94a66cb1ab81b61e220d5d5616f
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-12-04 16:31:25 -0300

    refactor: test AppStart script

--- scripts/AppStart.sh
+++ scripts/AppStart.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
+
+nvm install 16.18.1
+nvm use 16.18.1
+
 node app/index.js
