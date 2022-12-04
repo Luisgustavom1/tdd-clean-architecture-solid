73879bc569242933fcccd5003d91a1e13066c9bd
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-12-04 16:24:58 -0300

    refactor: update AfterInstall script

--- scripts/AfterInstallHook.sh
+++ scripts/AfterInstallHook.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 set -e
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
+
+nvm install 16.18.1
+nvm use 16.18.1
