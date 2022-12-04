a940360659fb2ef3fc4c870414cfc28a373cfc30
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-12-04 16:07:53 -0300

    refactor: update AfterInstall script

--- scripts/AfterInstallHook.sh
+++ scripts/AfterInstallHook.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
+ 
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 # install node version
 nvm use 16.18.1
 node --version
-
-# run appa
-node app/index.js
