c9da4e9e241191772570ef635e87f7923bfd44d2
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-12-09 08:18:27 -0300

    feat: add calendar to SurveyResult

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -5,6 +5,7 @@ import Footer from '@/presentation/components/footer'
 import Header from '@/presentation/components/header'
 import Styles from './survey-result-styles.scss'
 import { Loading } from '@/presentation/components/loading'
+import { Calendar } from '@/presentation/components/calendar'
 
 export const SurveyResult = () => {
   return (
@@ -12,11 +13,14 @@ export const SurveyResult = () => {
       <Header />
 
       <section className={Styles.contentWrap}>
-        <h2>Qual é seu framework web favorito</h2>
+        <hgroup className={Styles.titleContainer}>
+          <Calendar className={Styles.calendarWrap} date={new Date()} />
+          <h2>Qual é seu framework web favorito? Qual é seu framework web favorito? Qual é seu framework web favorito ito? Qual é seu framework web favorit ito? Qual é seu framework web favorit</h2>
+        </hgroup>
         <FlipMove className={Styles.answersList}>
           <li>
             <img src="https://images.unsplash.com/photo-1508138221679-760a23a2285b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80" />
-            <span className={Styles.answer}>ReactJs</span>
+            <span className={Styles.answer}>Rea ctJs</span>
             <span className={Styles.percent}>50%</span>
           </li>
           <li className={Styles.active}>
@@ -31,7 +35,7 @@ export const SurveyResult = () => {
           </li>
         </FlipMove>
         <Button variant='filled'>Voltar</Button>
-        {true && <Loading />}
+        {false && <Loading />}
       </section>
 
       <Footer />
