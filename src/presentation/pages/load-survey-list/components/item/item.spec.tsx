86279e564d294af14484cb75d584d45ddaad38f2
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-12-18 12:02:52 -0300

    fix: move Calendar test back to SurveyList

--- src/presentation/pages/load-survey-list/components/item/item.spec.tsx
+++ src/presentation/pages/load-survey-list/components/item/item.spec.tsx
@@ -11,18 +11,26 @@ const makeSut = (survey = mockSurveyModel()) => {
 describe('SurveyItem component', () => {
   it('Should render with correct values', () => {
     const survey = Object.assign(mockSurveyModel(), {
-      didAnswer: true
+      didAnswer: true,
+      date: new Date('2022-01-10T00:00:00')
     })
     makeSut(survey)
+    expect(screen.getByTestId('day').textContent).toBe('10')
+    expect(screen.getByTestId('month').textContent).toBe('jan')
+    expect(screen.getByTestId('year').textContent).toBe('2022')
     expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbUp)
     expect(screen.getByTestId('question').textContent).toBe(survey.question)
   })
 
   it('Should render with correct values', () => {
     const survey = Object.assign(mockSurveyModel(), {
-      didAnswer: false
+      didAnswer: false,
+      date: new Date('2019-05-05T00:00:00')
     })
     makeSut(survey)
+    expect(screen.getByTestId('day').textContent).toBe('05')
+    expect(screen.getByTestId('month').textContent).toBe('mai')
+    expect(screen.getByTestId('year').textContent).toBe('2019')
     expect(screen.getByTestId('icon')).toHaveProperty(
       'src',
       IconName.thumbDown
