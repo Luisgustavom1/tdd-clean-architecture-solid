c19f48ba88355f6847018117ee73ab28cfe98108
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-12-18 21:49:48 -0300

    test: ensure SurveyResult present correct result on success

--- src/presentation/hooks/use-survey-resuult.ts
+++ src/presentation/hooks/use-survey-resuult.ts
@@ -1,7 +1,38 @@
-export function useSurveyResult () {
-  return {
+import { LoadSurveyResult } from '@/domain/usecases'
+import { useEffect, useState } from 'react'
+
+interface ISurveyResult {
+  data: LoadSurveyResult.Model | null
+  isLoading: boolean
+  error: string
+}
+
+export function useSurveyResult (loadSurveyResult: LoadSurveyResult) {
+  const [surveyResult, setSurveyResult] = useState<ISurveyResult>({
+    data: null as LoadSurveyResult.Model,
     isLoading: false,
-    error: '',
-    surveys: null
+    error: ''
+  })
+
+  function updateSurveyResult (updatedSurveyResult: Partial<ISurveyResult>) {
+    setSurveyResult((prevState) => ({
+      ...prevState,
+      ...updatedSurveyResult
+    }))
   }
+
+  useEffect(() => {
+    updateSurveyResult({
+      isLoading: true
+    })
+    loadSurveyResult.load().then((newSurveyResult) => updateSurveyResult({
+      data: newSurveyResult
+    })).catch((error) => updateSurveyResult({
+      error: error.message
+    })).finally(() => updateSurveyResult({
+      isLoading: false
+    }))
+  }, [])
+
+  return surveyResult
 }
--- src/presentation/pages/survey-result/survey-result.spec.tsx
+++ src/presentation/pages/survey-result/survey-result.spec.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { render, screen, waitFor } from '@testing-library/react'
+import { act, render, screen, waitFor } from '@testing-library/react'
 import { SurveyResult } from '@/presentation/pages/survey-result/survey-result'
 import { ApiContext } from '@/presentation/contexts'
-import { LoadSurveyResultSpy, mockAccountModel } from '@/domain/test'
+import { LoadSurveyResultSpy, mockAccountModel, mockSurveyResultModel } from '@/domain/test'
 
-const makeSut = () => {
+const makeSut = (surveyResult = mockSurveyResultModel()) => {
   const loadSurveyResultSpy = new LoadSurveyResultSpy()
+  loadSurveyResultSpy.surveyResult = surveyResult
   render(
     <ApiContext.Provider
       value={{
@@ -25,6 +26,7 @@ describe('<SurveyResult />', () => {
   it('Should present 4 empty items on start', async () => {
     makeSut()
     const surveyList = screen.getByTestId('survey-result')
+    await waitFor(() => surveyList)
     expect(surveyList.childElementCount).toBe(0)
     expect(screen.queryByTestId('loading')).toBeNull()
     expect(screen.queryByTestId('error')).toBeNull()
@@ -36,5 +38,32 @@ describe('<SurveyResult />', () => {
       screen.getByTestId('survey-result')
     })
     expect(loadSurveyResultSpy.callsCount).toBe(1)
+  })
+
+  it('Should preset SurveyResult data on success', async () => {
+    const surveyResult = Object.assign(mockSurveyResultModel(), {
+      date: new Date('2022-01-10T00:00:00')
+    })
+    await act(() => {
+      makeSut(surveyResult)
+    })
+    expect(screen.getByTestId('day').textContent).toBe('10')
+    expect(screen.getByTestId('month').textContent).toBe('jan')
+    expect(screen.getByTestId('year').textContent).toBe('2022')
+    expect(screen.getByTestId('question').textContent).toBe(surveyResult.question)
+    expect(screen.getByTestId('answers').childElementCount).toBe(surveyResult.answers.length)
+    const answersWrap = screen.queryAllByTestId('answer-wrap')
+    expect(answersWrap[0].getAttribute('class')).toBe('active')
+    expect(answersWrap[1].getAttribute('class')).toBeFalsy()
+    const images = screen.queryAllByTestId('image')
+    expect(images[0].getAttribute('src')).toBe(surveyResult.answers[0].image)
+    expect(images[0].getAttribute('alt')).toBe(surveyResult.answers[0].answer)
+    expect(images[1]).toBeFalsy()
+    const answers = screen.queryAllByTestId('answer')
+    expect(answers[0].textContent).toBe(surveyResult.answers[0].answer)
+    expect(answers[1].textContent).toBe(surveyResult.answers[1].answer)
+    const percents = screen.queryAllByTestId('percent')
+    expect(percents[0].textContent).toBe(`${surveyResult.answers[0].percent}%`)
+    expect(percents[1].textContent).toBe(`${surveyResult.answers[1].percent}%`)
   })
 })
