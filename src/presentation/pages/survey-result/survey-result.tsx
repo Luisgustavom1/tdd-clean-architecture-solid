9fe52d36cef346a678b9612f29f8aac242494cdf
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-12-18 21:49:39 -0300

    feat: ensure SurveyResult present correct result on success

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import FlipMove from 'react-flip-move'
 import Button from '@/presentation/components/button'
 import Footer from '@/presentation/components/footer'
@@ -15,39 +15,29 @@ interface ISurveyResultProps {
 }
 
 export const SurveyResult = ({ loadSurveyResult }: ISurveyResultProps) => {
-  const { surveys, error, isLoading } = useSurveyResult()
-
-  useEffect(() => {
-    loadSurveyResult.load().then().catch()
-  })
+  const { data, error, isLoading } = useSurveyResult(loadSurveyResult)
   return (
     <div className={Styles.surveyResultWrap}>
       <Header />
 
       <section data-testid='survey-result' className={Styles.contentWrap}>
        {
-        surveys &&
+        data &&
           <>
             <hgroup className={Styles.titleContainer}>
-              <Calendar className={Styles.calendarWrap} date={new Date()} />
-              <h2>Qual é seu framework web favorito? Qual é seu framework web favorito? Qual é seu framework web favorito ito? Qual é seu framework web favorit ito? Qual é seu framework web favorit</h2>
+              <Calendar className={Styles.calendarWrap} date={data.date} />
+              <h2 data-testid='question'>{data.question}</h2>
             </hgroup>
-            <FlipMove className={Styles.answersList}>
-              <li>
-                <img src="https://images.unsplash.com/photo-1508138221679-760a23a2285b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80" />
-                <span className={Styles.answer}>Rea ctJs</span>
-                <span className={Styles.percent}>50%</span>
-              </li>
-              <li className={Styles.active}>
-                <img src="https://images.unsplash.com/photo-1508138221679-760a23a2285b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80" />
-                <span className={Styles.answer}>ReactJs</span>
-                <span className={Styles.percent}>50%</span>
-              </li>
-              <li>
-                <img src="https://images.unsplash.com/photo-1508138221679-760a23a2285b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80" />
-                <span className={Styles.answer}>ReactJs</span>
-                <span className={Styles.percent}>50%</span>
-              </li>
+            <FlipMove data-testid='answers' className={Styles.answersList}>
+              {
+                data.answers.map((answer) => (
+                  <li data-testid='answer-wrap' className={answer.isCurrentAccountAnswer ? Styles.active : ''} key={answer.answer}>
+                    {answer.image && <img data-testid='image' alt={answer.answer} src={answer.image} />}
+                    <span data-testid='answer' className={Styles.answer}>{answer.answer}</span>
+                    <span data-testid='percent' className={Styles.percent}>{answer.percent}%</span>
+                  </li>
+                ))
+              }
             </FlipMove>
             <Button variant='filled'>Voltar</Button>
             {isLoading && <Loading />}
