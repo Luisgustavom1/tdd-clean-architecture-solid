fd8b8fb88180c883135499d2a05726b6d1f8d1e6
luisgustavom1 <luisgustavomacedo13@gmail.com>
2023-05-06 12:08:44 -0300

    fix: correct hook scripts and add before install hook

--- scripts/AppStart.sh
+++ scripts/AppStart.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
-
-nvm install 16.18.1
-nvm use 16.18.1
-npm install -g pnpm
-
-cd ./home/ec2-user/app
-
-pnpm install
-pnpm build
-
 node /home/ec2-user/app/index.js
---
+++ scripts/BeforeInstallHook.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt-get update
+wget -qO- https://get.pnpm.io/install.sh | sh -
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
